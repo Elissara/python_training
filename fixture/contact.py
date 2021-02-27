cbd50c624fbf05c88ed340ab3aa230d0e645d4cb
Elissara <68391346+Elissara@users.noreply.github.com>
2021-02-27 13:39:04 +0300

    Доработки по замечаниям

--- fixture/contact.py
+++ fixture/contact.py
@@ -10,25 +10,22 @@ class ContactHelper:
     def create(self, contact):
         wd = self.app.wd
         self.open_contact_create_page()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        self.input_values(contact)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.return_to_home_page()
 
     def change(self, contact):
         wd = self.app.wd
-        # self.open_home_page()
         # select first group
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_xpath("(//img[@alt='Edit'])[4]").click()
+        self.input_values(contact)
+        wd.find_element_by_xpath("(//input[@name='update'])[1]").click()
+        self.return_to_home_page()
+
+    def input_values(self, contact):
+        wd = self.app.wd
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -38,9 +35,6 @@ class ContactHelper:
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
         wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # submit group creation
-        wd.find_element_by_name("update").click()
-        self.return_to_home_page()
 
     def delete_first_contact(self):
         wd = self.app.wd
--- fixture/group.py
+++ fixture/group.py
@@ -15,16 +15,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
+        self.input_values(group)
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
@@ -35,6 +26,13 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_name("edit").click()
+        self.input_values(group)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def input_values(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -44,9 +42,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
