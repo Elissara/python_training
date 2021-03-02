d6e203fa54e2d437943b9d8956d5479f5d773ce4
Elissara <68391346+Elissara@users.noreply.github.com>
2021-03-02 19:18:03 +0300

    Оптимизированы операции переходов между страницами

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,8 @@ class GroupHelper:
 
     def open_group_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -79,7 +80,8 @@ class GroupHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
