54c87ae7681372161da91a981cbb8ce14bd84546
Elissara <68391346+Elissara@users.noreply.github.com>
2021-03-12 20:36:50 +0300

    Реализован тест для случайно выбранного контакта, сверяющий информацию на главной странице с информацией в форме редактирования

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 
 class ContactHelper:
@@ -15,7 +16,7 @@ class ContactHelper:
         self.open_contact_create_page()
         self.fill_contact_form(contact)
         wd.find_element_by_name("submit").click()
-        self.return_to_home_page()
+        self.open_home_page()
         self.contact_cache = None
 
     def change(self):
@@ -28,7 +29,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         self.fill_contact_form(contact)
         wd.find_element_by_name("update").click()
-        self.return_to_home_page()
+        self.open_home_page()
         self.contact_cache = None
 
     def fill_contact_form(self, contact):
@@ -63,7 +64,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         # submit deletion
         wd.switch_to_alert().accept()
-        self.return_to_home_page()
+        self.open_home_page()
         self.contact_cache = None
 
     def modify_first_contact(self):
@@ -79,17 +80,17 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         # submit modification
         wd.find_element_by_name("update").click()
-        self.return_to_home_page()
+        self.open_home_page()
         self.contact_cache = None
 
-    def return_to_home_page(self):
+    def open_home_page(self):
         wd = self.app.wd
         if not (wd.current_url.endswith("/") and len(wd.find_elements_by_name("add")) > 0):
             wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
-        self.return_to_home_page()
+        self.open_home_page()
         return len(wd.find_elements_by_name("selected[]"))
 
     contact_cache = None
@@ -97,10 +98,63 @@ class ContactHelper:
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
-            self.return_to_home_page()
+            self.open_home_page()
             self.contact_cache = []
-            for element in wd.find_elements_by_xpath("//tr[@name='entry']"):
-                text = element.text
-                id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(lastname=text, firstname=text, id=id))
+            for row in wd.find_elements_by_name("entry"):
+                cells = row.find_elements_by_tag_name("td")
+                lastname = cells[1].text
+                firstname = cells[2].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                all_phones = cells[5].text
+                all_emails = cells[4].text
+                address = cells[3].text
+                self.contact_cache.append(Contact(id=id, lastname=lastname, firstname=firstname, all_phones_from_homepage=all_phones,
+                                                 all_emails_from_homepage=all_emails, address=address))
         return list(self.contact_cache)
+
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id, homephone=homephone, workphone=workphone,
+                       mobilephone=mobilephone, secondaryphone=secondaryphone, address=address,
+                       email=email, email2=email2, email3=email3)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+
+        return Contact(homephone=homephone, workphone=workphone,
+                       mobilephone=mobilephone, secondaryphone=secondaryphone)
+
+
--- model/contact.py
+++ model/contact.py
@@ -3,10 +3,23 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, nickname=None, id=None):
+    def __init__(self, firstname=None, lastname=None, nickname=None, id=None, address=None,
+                 homephone=None, mobilephone=None, workphone=None,  secondaryphone=None, all_phones_from_homepage=None,
+                 email=None, email2=None, email3=None, all_emails_from_homepage=None):
+
         self.firstname = firstname
         self.lastname = lastname
         self.nickname = nickname
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.secondaryphone = secondaryphone
+        self.all_phones_from_homepage = all_phones_from_homepage
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.all_emails_from_homepage = all_emails_from_homepage
         self.id = id
 
     def __repr__(self):
