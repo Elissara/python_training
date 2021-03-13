c4b6a492a8b50a9f1d3817b3360e90600d7a91ae
Elissara <68391346+Elissara@users.noreply.github.com>
2021-03-13 21:00:18 +0300

    Реализовано использование случайно сгенерированныч строк для свойств создаваемых групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -23,7 +23,7 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname)
+        return "%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.nickname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
