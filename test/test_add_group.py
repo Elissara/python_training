1b4ffdb69fba04e2eb9cfcc6b2c6359d96e20a16
Elissara <68391346+Elissara@users.noreply.github.com>
2021-03-13 21:05:57 +0300

    Реализовано использование случайно сгенерированныч строк для свойств создаваемых групп и контактов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import time
 import pytest
 import random
 import string
@@ -20,6 +21,7 @@ testdata = [
 def test_add_group(app, group):
         old_groups = app.group.get_group_list()
         app.group.create(group)
+        time.sleep(3)
         assert len(old_groups) + 1 == app.group.count()
         new_groups = app.group.get_group_list()
         old_groups.append(group)
