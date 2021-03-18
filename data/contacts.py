aae00b64109b98f3ff9c0965278b70ddf81ae584
Elissara <68391346+Elissara@users.noreply.github.com>
2021-03-18 21:11:01 +0300

    Реализована загрузка тестовых данных из файлов JSON и параметризация тестов

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="fifstname1", lastname="lastname1", nickname="nickname1"),
+    Contact(firstname="fifstname2", lastname="lastname2", nickname="nickname2")
+]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import time
-import pytest
-from data.add_groups import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, json_groups):
+        group = json_groups
         old_groups = app.group.get_group_list()
         app.group.create(group)
         time.sleep(3)
